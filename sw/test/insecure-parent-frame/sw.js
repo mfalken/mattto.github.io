b2136c64b359f2d983fe975a2acc40c9a8c2438a
Matt Falkenhagen <falken@chromium.org>
2016-06-14 16:26:27 +0900

    add claim test

--- sw/test/insecure-parent-frame/sw.js
+++ sw/test/insecure-parent-frame/sw.js
@@ -3,3 +3,9 @@ self.addEventListener('fetch', e => {
       e.respondWith(new Response('intercepted by SW'));
     }
   });
+
+self.addEventListener('message', e => {
+    e.waitUntil(clients.claim()
+      .then(() => e.data.port.postMessage('claim finished'))
+      .catch(() => e.data.port.postMessage('claim failed')));
+  });
