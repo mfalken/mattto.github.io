8d4dbe3ccdd00863911d5a5a45f3903d2e4c0995
Matt Falkenhagen <falken@chromium.org>
2016-09-27 11:16:56 +0900

    Add navigation-preload test

---
+++ sw/test/navigation-preload/normal_sw.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview Description of this file.
+ */
+function busySleep(millisec)
+{
+    var start = new Date();
+    var date = null;
+    do {
+      date = new Date();
+    } while(date - start < millisec);
+}
+/*
+// Simulate heavy Service Worker script.
+busySleep(500);
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  if (!event.preloadResponse) {
+    console.log("event.preloadResponse is null");
+    return;
+  }
+  if (event.request.url.indexOf('no_respondWith.html') != -1) {
+    return;
+  }
+  if (event.request.url.indexOf('respondWith_preloadResponse.html') != -1) {
+    event.respondWith(event.preloadResponse);
+    return;
+  }
+  if (event.request.url.indexOf('respondWith_new_Response.html') != -1) {
+    event.respondWith(new Response('Hello world'));
+    return;
+  }
+});
+*/
+
