04811eb296ef7601d752103271515826f7430457
Matt Falkenhagen <falken@chromium.org>
2019-03-20 17:34:44 +0900

    Updates

---
+++ precache-manifest.432477f8db8560bf36009a1dfff906b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dinosaur-match/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "33c41ceabd3e141e035b",
+    "url": "/dinosaur-match/static/js/runtime~main.0235f67f.js"
+  },
+  {
+    "revision": "f31f965c393393cac87b",
+    "url": "/dinosaur-match/static/js/main.19122315.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/dinosaur-match/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "f31f965c393393cac87b",
+    "url": "/dinosaur-match/static/css/main.a5fb12fb.chunk.css"
+  },
+  {
+    "revision": "e290c40df23a1f5fda9907aff397cd70",
+    "url": "/dinosaur-match/index.html"
+  }
+];
